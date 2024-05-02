9080d5f64320c1aaa409d4eac19296bec7647115
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-05-02 09:48:34 -0400

    Learning about Image

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import React from "react";
+import styles from "./about.module.css";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/18404269/pexels-photo-18404269/free-photo-of-capsules-of-ferris-wheel.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+          width={500}
+          height={500}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
