f6bbe6fc03006eade03f6d96a369aa0d8a813dcf
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-05-02 10:33:21 -0400

    Home Page Design

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>nicholas</div>
+      <div className={styles.text}>NicholasCloud4 © All rights reserved.</div>
+    </div>
+  );
 };
 
 export default Footer;
