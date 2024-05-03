7f491012c987af6c5f0d8748f5c66c6cf9dc6e6f
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-05-03 12:48:32 -0400

    Working with MONGODB

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -26,28 +26,14 @@ const SinglePostPage = async ({ params }) => {
   const post = await getPost(slug);
   return (
     <div className={styles.container}>
-      <div className={styles.imgContainer}>
-        <Image
-          src={
-            "https://images.pexels.com/photos/19086280/pexels-photo-19086280/free-photo-of-tower-of-st-stephens-cathedral-in-vienna.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-          }
-          alt=""
-          fill
-          className={styles.img}
-        />
-      </div>
+      {post.img && (
+        <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img} />
+        </div>
+      )}
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>{post?.title}</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
-          <Image
-            src={
-              "https://images.pexels.com/photos/19086280/pexels-photo-19086280/free-photo-of-tower-of-st-stephens-cathedral-in-vienna.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            }
-            alt=""
-            className={styles.avatar}
-            width={50}
-            height={50}
-          />
           {post && (
             <Suspense fallback={<div>Loading...</div>}>
               <PostUser userId={post.userId} />
@@ -56,10 +42,12 @@ const SinglePostPage = async ({ params }) => {
 
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
-            <span className={styles.detailValue}>05.02.2024</span>
+            <span className={styles.detailValue}>
+              {post.createdAt.toString().slice(3, 16)}
+            </span>
           </div>
         </div>
-        <div className={styles.content}>{post?.body}</div>
+        <div className={styles.content}>{post.desc}</div>
       </div>
     </div>
   );
--- src/components/postUser/postUser.jsx
+++ src/components/postUser/postUser.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./postUser.module.css";
 import { getUser } from "@/lib/data";
+import Image from "next/image";
 
 //FETCH WITH API
 // const getData = async (userId) => {
@@ -17,16 +18,25 @@ import { getUser } from "@/lib/data";
 // };
 
 const PostUser = async ({ userId }) => {
-  //FETCH WITH API
-  //   const user = await getData(userId);
+  // FETCH DATA WITH AN API
+  // const user = await getData(userId);
 
-  //FETCH WITHOUT API
+  // FETCH DATA WITHOUT AN API
   const user = await getUser(userId);
 
   return (
     <div className={styles.container}>
-      <span className={styles.title}>Author</span>
-      <span className={styles.username}>{user.username}</span>
+      <Image
+        className={styles.avatar}
+        src={user.img ? user.img : "/noavatar.png"}
+        alt=""
+        width={50}
+        height={50}
+      />
+      <div className={styles.texts}>
+        <span className={styles.title}>Author</span>
+        <span className={styles.username}>{user.username}</span>
+      </div>
     </div>
   );
 };
